653c94e3d4a1e3f956154caf01a3e0e8ee132905
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-11-09 15:24:30 -0800

    WIP

--- scripts/plot_radar_diagram.py
+++ scripts/plot_radar_diagram.py
@@ -9,7 +9,7 @@
 
 import chart_studio.plotly as py
 import plotly.graph_objs as go  # https://plotly.com/python-api-reference/plotly.graph_objects.html
-from typing import TypedDict
+from typing import TypedDict, Any
 import numpy as np  # DELETE
 
 from pg import *
@@ -45,13 +45,15 @@ class Ratings(TypedDict):
 
 
 def cull_ratings(raw_in: dict) -> Ratings:
-    return {
-        "proportionality": raw_in["score_proportionality"],
-        "competitiveness": raw_in["score_competitiveness"],
-        "minority_opportunity": raw_in["score_minorityRights"],
-        "compactness": raw_in["score_compactness"],
-        "splitting": raw_in["score_splitting"],
+    r: Ratings = {
+        "proportionality": int(raw_in["score_proportionality"]),
+        "competitiveness": int(raw_in["score_competitiveness"]),
+        "minority_opportunity": int(raw_in["score_minorityRights"]),
+        "compactness": int(raw_in["score_compactness"]),
+        "splitting": int(raw_in["score_splitting"]),
     }
+
+    return r
 
 
 current_ratings: Ratings = cull_ratings(load_json(current_file))
@@ -60,8 +62,31 @@ compare_ratings: Ratings = cull_ratings(load_json(compare_file))
 
 ### PLOT RADAR DIAGRAM ###
 
-current_title: str = f"{xx}{yy} {type} {current_subtype}"
-compare_title: str = f"{xx}{yy} {type} {compare_subtype}"
+
+def text_position(ratings: list[int], move_value: int) -> list[str]:
+    positions: list[str] = [
+        "top right" if (ratings[0] < move_value) else "top left",
+        "top center" if (ratings[1] < move_value) else "middle left",
+        "top left" if (ratings[2] < move_value) else "middle right",
+        "bottom left" if (ratings[3] < move_value) else "middle right",
+        "bottom center" if (ratings[4] < move_value) else "middle left",
+        "top right" if (ratings[5] < move_value) else "top left",
+    ]
+
+    return positions
+
+
+current_name: str = f"{xx}{yy} {type} {current_subtype}"
+compare_name: str = f"{xx}{yy} {type} {compare_subtype}"
+
+
+class Plan(TypedDict):
+    name: str
+    ratings: Ratings
+
+
+current_plan: Plan = {"name": current_name, "ratings": current_ratings}
+compare_plan: Plan = {"name": compare_name, "ratings": compare_ratings}
 
 # TODO - DELETE
 def example_plot() -> None:
@@ -89,259 +114,80 @@ def example_plot() -> None:
     fig.write_image("content/" + plot_file + ".png")
 
 
-# TODO - plot radar diagram
-def plot_radar_diagram() -> None:
+def plot_radar_diagram(current: Plan, compare: Plan) -> None:
     """
     https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html
-    https://plotly.com/python/renderers/
     https://plotly.com/python/static-image-export/
     """
 
-    traces: list = []
+    # TODO
+    # * Border not completing
+    # * Values underneath?
 
-    layout: py.Layout = go.Layout(
-        title="Pairwise Comparison",
-        # width=svSize,
-        # height=svSize,
-        # xaxis=dict(
-        #     title="Vote fraction",
-        #     range=x_range,
-        #     dtick=0.05,
-        #     tickformat=".2f",
-        #     # tickformat=".0%",
-        #     showgrid=True,
-        #     gridcolor="lightgrey",
-        # ),
-        # yaxis=dict(
-        #     title="Seat fraction",
-        #     range=y_range,
-        #     scaleanchor="x",
-        #     scaleratio=1,
-        #     dtick=0.05,
-        #     tickformat=".2f",
-        #     # tickformat=".0%",
-        #     showgrid=True,
-        #     gridcolor="lightgrey",
-        # ),
-        # dragmode="zoom",
-        # hovermode="closest",
-        # showlegend=False,
-        # paper_bgcolor=bgcolor,
-        # plot_bgcolor=bgcolor,
+    traces: list = []
+    theta: list[str] = [
+        "Proportionality",
+        "Competitiveness",
+        "Minority",
+        "Compactness",
+        "Splitting",
+    ]
+
+    # Current trace
+
+    current_r: list[int] = [x for x in current["ratings"].values()]
+    current_r += current_r[:1]
+
+    current_trace: py.Scatterpolar = go.Scatterpolar(
+        name=current["name"],
+        mode="lines+markers+text",
+        r=current_r,
+        theta=theta,
+        fill="toself",
+        text="",  # Suppress the current ratings
+        fillcolor="rgba(44, 160, 44, 0.5)",
+        marker={"color": "black"},
+        line={"color": "black"},
     )
 
-    fig: py.Figure = go.Figure(data=traces, layout=layout)
-    py.plot(fig, filename=plot_file)
+    # Compare trace
 
+    compare_r: list[int] = [x for x in compare["ratings"].values()]
+    compare_r += compare_r[:1]
+    compare_positions: list[str] = text_position(compare_r, 80)
 
-plot_radar_diagram()
+    compare_trace: py.Scatterpolar = go.Scatterpolar(
+        name=compare["name"],
+        mode="lines+markers+text",
+        r=compare_r,
+        theta=theta,
+        # TODO
+        # dimensions: dimensions, TODO - ???
+        fill="toself",
+        fillcolor="rgba(255, 127, 14, 0.5)",
+        text=compare_r,
+        textposition=compare_positions,
+    )
 
-pass
+    # TODO: HACK - Add ghost traces???
 
-"""
-// From dra-client:
+    # traces.append(compare_ghost)
+    # traces.append(current_ghost)
+    traces.append(compare_trace)
+    traces.append(current_trace)
 
-export function renderRadarDiagram(config: RadarConfig): void
-{
-  const bLegend: boolean = config.radarLegendOn;
-  const bValues: boolean = config.radarValuesOn;
+    # TODO - Decide on title template
+    title: str = "Ratings: " + current["name"]
+    font_size: int = 16
+    title_x: float = 0.5
 
-  // Drive the internal configuration switches off of the calling context
-  const radarCompareOn: boolean = (config.context >= RadarContext.CompareTwo) ? true : false;
-  // const radarTitleOn: boolean = true; <<< title set below using context
-  const radarLabelsOn: boolean = (config.context == RadarContext.ComparePairs) ? false : true;
-
-  let bCompare: boolean = radarCompareOn && config.compareSessionProps ? true : false;
-
-  if (!config.profile || !config.scorecard)
-    return;
-  
-  const name = config.profile.name;
-  let compareName = '';
-
-  // RADAR DIAGRAM
-
-  let radarTraces: any[] = [];
-  let radarLayout: any = {};
-  let radarConfig: any = {};
-  {
-    // Define trace for the current map
-
-    let compareTrace: any;
-    let compareGhost: any;
-    let currentGhost: any;
-    let ratings = bCompare ? sessionPropsToRatings(config.compareSessionProps) : null;
-    bCompare = bCompare && !!ratings;     // Only compare if something has been selected to compare
-
-    // No partisan data - Revised the labels
-    // Use full labels if labels are on; otherwise elide them to one character
-    const dimensions: string[] = config.bHidePartisanData ? 
-    [
-      Ratings.equitable.label,
-      Ratings.compact.label,
-      Ratings.cohesive.label,
-      Ratings.equitable.label
-    ] : 
-    [
-      Ratings.unbiased.label,
-      Ratings.competitive.label,
-      Ratings.equitable.label,
-      Ratings.compact.label,
-      Ratings.cohesive.label,
-      Ratings.unbiased.label
-    ];
-    const shortLabels = config.bHidePartisanData ? [ "M", "C'", "S", "M" ] : [ "P", "C", "M", "C'", "S", "P" ];
-    const theta = radarLabelsOn ? dimensions : shortLabels;
-
-    // No partisan data - Revised the current data
-    const rCurrent: number[] = config.bHidePartisanData ?
-    [
-      config.scorecard.minority.score,
-      config.scorecard.compactness.score,
-      config.scorecard.splitting.score,
-      config.scorecard.minority.score
-    ] :
-    [
-      config.scorecard.partisan.bias.score,
-      config.scorecard.partisan.responsiveness.score,
-      config.scorecard.minority.score,
-      config.scorecard.compactness.score,
-      config.scorecard.splitting.score,
-      config.scorecard.partisan.bias.score,
-    ];
-    const reqsAnnotation: string = 'Requirements: ' + (requirementsPass(config.reqsChecklist, config.targetPopEqual) ? 'Met' : 'Not met');
-
-    // No partisan data - Revised the text positions
-    const currentPositions: string[] = getTextPosition(rCurrent, 80, config.bHidePartisanData);
-
-    let currentHover: string[] = [];
-    for (var i = 0; i < dimensions.length; i++)
-    {
-      currentHover.push(`${rCurrent[i]}: ${dimensions[i]}<extra></extra>`);
-    }
-
-    let currentTrace: any =
-      {
-        name: name,
-        type: 'scatterpolar',
-        mode: 'lines+markers+text',
-        r: rCurrent,
-        theta: theta,
-        fill: 'toself',
-        text: (bValues && !bCompare) ? rCurrent : '',
-        textposition: (bValues && !bCompare) ? currentPositions : '',
-        hoverinfo: (bValues && !bCompare) ? 'none' : 'points',
-        hovertemplate: (bValues && !bCompare) ? '' : currentHover,
-        // hovertemplate: (bValues && !bCompare) ? '' : "%{r}<extra></extra>",
-        showlegend: false
-      };
-
-    // Change the default coloring of the current/base map <<< for all scenarios/cases
-    currentTrace.fillcolor = 'rgba(44, 160, 44, 0.5)';
-    currentTrace.marker = {color: 'black'};
-    currentTrace.line = {color: 'black'};
-  
-    if (bCompare)
-    {
-      // Get properties of the map to compare to
-      compareName = (config.compareOrder ? String(config.compareOrder) + ' – ' : '') + config.compareSessionProps.name;
-
-      // No partisan data - Revised the compare data
-      const rCompare = config.bHidePartisanData ?
-      [
-        ratings.minorityRights,
-        ratings.compactness,
-        ratings.splitting,
-        ratings.minorityRights
-      ] :
-      [
-        ratings.proportionality,
-        ratings.competitiveness,
-        ratings.minorityRights,
-        ratings.compactness,
-        ratings.splitting,
-        ratings.proportionality
-      ];
-
-      const comparePositions: string[] = getTextPosition(rCompare, 80, config.bHidePartisanData);
-
-      let compareHover: string[] = [];
-      for (var i = 0; i < dimensions.length; i++)
-      {
-        compareHover.push(`${rCompare[i]}: ${dimensions[i]}<extra></extra>`);
-      }
-
-      compareTrace =
-      {
-        name: compareName,
-        type: 'scatterpolar',
-        mode: 'lines+markers+text',
-        r: rCompare,
-        theta: theta,
-        dimensions: dimensions,
-        fill: 'toself',
-        fillcolor: 'rgba(255, 127, 14, 0.5)',
-        text: rCompare,
-        textposition: comparePositions,
-        hoverinfo: 'points',
-        hovertemplate: compareHover,
-        // hoverinfo: 'none',
-        // hovertemplate: '',
-        showlegend: false
-      };
-
-      // HACK - Add traces to eliminate the weird Aa in the legend
-      compareGhost = Util.deepCopy(compareTrace);
-      delete compareGhost.text;
-      delete compareGhost.textposition;
-      delete compareGhost.hoverinfo;
-      delete compareGhost.hovertemplate;
-      compareGhost.mode = 'lines+markers';
-      compareGhost.showlegend = bLegend;
-
-      currentGhost = Util.deepCopy(currentTrace);
-      delete currentGhost.text;
-      delete currentGhost.textposition;
-      delete currentGhost.hoverinfo;
-      delete currentGhost.hovertemplate;
-      currentGhost.mode = 'lines+markers';
-      currentGhost.showlegend = bLegend;
-
-      // Push the traces in the order to achieve the desired behavior
-      radarTraces.push(compareGhost);
-      radarTraces.push(currentGhost);
-      radarTraces.push(compareTrace);
-    }
-
-    radarTraces.push(currentTrace);
-
-    let radarTitle: string = 'Ratings: ' + name;
-    let fontSize: number = 16;
-    let titleX = 0.5;           // Centert the title 
-
-    if (config.context == RadarContext.ComparePairs) {
-      radarTitle = compareName;
-
-      const maxTitleLength = 50;
-      if (maxTitleLength < compareName.length) {
-        radarTitle = compareName.substring(0, maxTitleLength) + ' ...';
-      }
-
-      fontSize = 12;
-    }
-
-    const radarAnnotations = bCompare ? [] :
-    [
-      {
-        x: 0,
-        y: 0,
-        text: bAnnotateDiagram ? reqsAnnotation : '',
-        showarrow: false,
-        xshift: 65,
-        yshift: -20
-      }
-    ];
-
+    layout: py.Layout = go.Layout(
+        title=dict(text=title, x=title_x, font_size=font_size),
+        polar=dict(radialaxis=dict(visible=True, range=[0, 100], showticklabels=False)),
+        showlegend=False,
+    )
+    """
+    TODO
     radarLayout = {
       title: {
         text: radarTitle,
@@ -370,22 +216,12 @@ export function renderRadarDiagram(config: RadarConfig): void
       paper_bgcolor: MA.appBackgroundColor,
       plot_bgcolor: MA.appBackgroundColor
     };
+    """
 
-    // Configure hover menu options & behavior
+    fig: py.Figure = go.Figure(data=traces, layout=layout)
+    py.plot(fig, filename=plot_file)
 
-    radarConfig = {
-      toImageButtonOptions: {
-        format: 'png', // one of png, svg, jpeg, webp
-        filename: 'ratings-diagram'
-      },
-      // Remove all the plotly hover commands, except download
-      modeBarButtonsToRemove: ['zoom2d', 'select2d', 'lasso2d', 'toggleHover'],
-      displayModeBar: true,
-      displaylogo: false,
-      responsive: true
-    };
-    Plotly.newPlot(config.div, radarTraces, radarLayout, radarConfig);
-  }
-}
 
-"""
+plot_radar_diagram(current_plan, compare_plan)
+
+pass
