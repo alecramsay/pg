1baa03887a0fd5c804dde47cce4f195d0dcc162f
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-11-15 06:09:00 -0800

    WIP for duplicating maps

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -6,11 +6,10 @@
 #
 # scripts/run_batch.sh
 #
+# -i 052e108a-b9f9-4c83-a135-5d5a839e176d
 
-scripts/pull_map_ratings.sh NC Congress Official 6e8268a4-3b9b-4140-8f99-e3544a2f0816
+ROOT=/Users/alecramsay/iCloud/dev/
+SCRIPT_DIR=$ROOT\dra-cli
 
-scripts/pull_map_ratings.sh NC Congress Proportional 7cfcec86-b8bf-41fa-a6c2-0be9b3799747
-scripts/pull_map_ratings.sh NC Congress Competitive 40178c75-312d-4789-a457-d609b0d8a043
-scripts/pull_map_ratings.sh NC Congress Minority 3b168396-6666-4167-830e-4500bde459fe
-scripts/pull_map_ratings.sh NC Congress Compact 3bacb15d-e014-4764-9e8c-f238eb687d50
-scripts/pull_map_ratings.sh NC Congress Splitting 82e6303c-e842-469b-b364-918bcacf9ef3
+$SCRIPT_DIR/draclient.js -u alec@davesredistricting.org -x Front.Seat -i 052e108a-b9f9-4c83-a135-5d5a839e176d -d
+$SCRIPT_DIR/draclient.js -u alec@davesredistricting.org -x Front.Seat -N "NC 2022 test name" -D "Copy of ..." -L [+PG-OFFICIAL]
