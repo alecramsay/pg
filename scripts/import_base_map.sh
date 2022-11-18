563a33b02501d3f3fe65f6039f8440f0bda94a95
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-11-17 16:35:38 -0800

    Made sure import works

--- scripts/import_base_map.sh
+++ scripts/import_base_map.sh
@@ -4,7 +4,7 @@
 #
 # For example:
 #
-# scripts/import_base_map.sh NC Congress blocks
+# scripts/import_base_map.sh NC Congress block
 #
 
 XX=$1
@@ -15,4 +15,5 @@ UNITS=$3
 SCRIPT_DIR=/Users/alecramsay/iCloud/dev/dra-cli
 CSV_FILE=/Users/alecramsay/iCloud/dev/baseline/results/$XX/$XX\_$YYYY\_$PLAN_TYPE\_$UNITS.csv
 
-$SCRIPT_DIR/importmap.js -u alec@davesredistricting.org -f $CSV_FILE -T $PLAN_TYPE -N "$XX $YYYY $PLAN_TYPE (Baseline)" -D "Baseline map, $UNITS" -L "PG-BASELINE"
+echo $SCRIPT_DIR/importmap.js -u alec@davesredistricting.org -f $CSV_FILE -T $PLAN_TYPE -N "$XX $YYYY $PLAN_TYPE (Baseline)" -D "Baseline map" -L "PG-BASELINE"
+$SCRIPT_DIR/importmap.js -u alec@davesredistricting.org -f $CSV_FILE -T $PLAN_TYPE -N "$XX $YYYY $PLAN_TYPE (Baseline)" -D "Baseline map" -L "PG-BASELINE"
