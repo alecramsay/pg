3e7c2eab0cea01825cb324ddfae8201c3d23688b
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-06-26 13:20:53 -0700

    Tweaked import map script

--- scripts/import_base_map.sh
+++ scripts/import_base_map.sh
@@ -10,7 +10,8 @@
 XX=$1
 YYYY=2020
 YY=20
-PLAN_TYPE=Congress
+PLAN_NAME=Congress
+PLAN_TYPE=$(echo "$PLAN_NAME" | tr '[:upper:]' '[:lower:]')
 P=C
 # UNITS=vtd
 LABEL=baseline
@@ -19,5 +20,5 @@ ITER=100
 SCRIPT_DIR=/Users/alecramsay/iCloud/dev/dra-cli
 CSV_FILE=/Users/alecramsay/iCloud/dev/baseline/maps/$XX/$XX$YY$P\_$LABEL\_$ITER.csv
 
-echo $SCRIPT_DIR/importmap.js -u alec@davesredistricting.org -f $CSV_FILE -T $PLAN_TYPE -N "$XX $YYYY $PLAN_TYPE - Baseline" -D "Baseline map" -L "PG-BASELINE"
-$SCRIPT_DIR/importmap.js -u alec@davesredistricting.org -f $CSV_FILE -T $PLAN_TYPE -N "$XX $YYYY $PLAN_TYPE - Baseline" -D "Baseline map" -L "PG-BASELINE"
+echo $SCRIPT_DIR/importmap.js -u alec@davesredistricting.org -f $CSV_FILE -T $PLAN_TYPE -N "$XX $YYYY $PLAN_NAME - Baseline" -D "Baseline map" -L "PG-BASELINE"
+$SCRIPT_DIR/importmap.js -u alec@davesredistricting.org -f $CSV_FILE -T $PLAN_TYPE -N "$XX $YYYY $PLAN_NAME - Baseline" -D "Baseline map" -L "PG-BASELINE"
