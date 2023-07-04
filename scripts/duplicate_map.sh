1a1b2d185b458b79677a06774eaf816ef55d067c
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-07-04 08:49:23 -0700

    Re-analyzed MA

--- scripts/duplicate_map.sh
+++ scripts/duplicate_map.sh
@@ -19,7 +19,7 @@ ROOT=/Users/alecramsay/iCloud/dev/
 SCRIPT_DIR=$ROOT\dra-cli
 
 USER=alec@davesredistricting.org
-PW=???
+PW=??? # NOTE - This doesn't seem to work anymore - 07/04/23.
 
 NAME="$XX $YYYY $PLAN_TYPE - $GROUP"
 DESC="Copy of $XX $GROUP"
