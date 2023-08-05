10d35ca73b7cc0f12f16bf891cf6bb8b91897dd6
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-08-05 06:58:46 -0700

    Integrated Steve Marx's screenshot code

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -43,66 +43,69 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 var _this = this;
 var puppeteer = require('puppeteer');
+// TODO - add command line args:
+// - map guid 
+// - image path
+// const [url, pathInput] = process.argv.slice(2);
+// if (url === undefined) {
+//   console.log("Usage: node mapscreenshot.js <url> [<screenshot PNG path>]");
+// }
+var url = 'https://davesredistricting.org/maps#viewmap::bbd90d8a-b4c3-4875-8ffe-4f931e141211';
+// const url = 'https://www.nakedcapitalism.com/';
+// const url = 'https://davesredistricting.org/';
+var screenshotPath = "/Users/alecramsay/Downloads/screenshot.png";
+// const screenshotPath = pathInput ?? "screenshot.png";
 (function () { return __awaiter(_this, void 0, void 0, function () {
-    var browser, page, VWIDTH, VHEIGHT, VIEWPORT, url, image, body, err_1;
+    var browser, page, canvas, e_1;
     return __generator(this, function (_a) {
         switch (_a.label) {
-            case 0: return [4 /*yield*/, puppeteer.launch()];
+            case 0: return [4 /*yield*/, puppeteer.launch({ headless: "new" })];
             case 1:
                 browser = _a.sent();
-                return [4 /*yield*/, browser.newPage()];
+                _a.label = 2;
             case 2:
-                page = _a.sent();
-                VWIDTH = 1382;
-                VHEIGHT = 790;
-                VIEWPORT = { width: VWIDTH, height: VHEIGHT };
-                return [4 /*yield*/, page.setViewport(VIEWPORT)];
+                _a.trys.push([2, 9, 10, 12]);
+                return [4 /*yield*/, browser.newPage()];
             case 3:
-                _a.sent();
-                url = 'https://davesredistricting.org/';
-                image = '/Users/alecramsay/Downloads/screenshot.png';
-                _a.label = 4;
+                page = _a.sent();
+                // await page.setViewport({width: 2560, height: 1600});
+                // await page.setViewport({width: 1998, height: 1394});
+                return [4 /*yield*/, page.setViewport({ width: 1280, height: 1024 })];
             case 4:
-                _a.trys.push([4, 7, , 8]);
-                return [4 /*yield*/, page.goto(url, {
-                        waitUntil: 'domcontentloaded'
-                        // waitUntil: 'networkidle0'
-                        // waitUntil: 'networkidle2'
-                        // timeout: 10000
-                    })];
+                // await page.setViewport({width: 2560, height: 1600});
+                // await page.setViewport({width: 1998, height: 1394});
+                _a.sent();
+                console.log("Loading the page. This can take 30 seconds or so... ");
+                return [4 /*yield*/, page.goto(url, { waitUntil: ['load', 'domcontentloaded', 'networkidle0'], timeout: 0 })];
             case 5:
                 _a.sent();
-                return [4 /*yield*/, page.$("body")];
+                return [4 /*yield*/, page.addStyleTag({
+                        content: '.MuiModal-backdrop, .MuiDialog-container, .MuiButtonBase-root, .MuiTypography-root, .mapboxgl-control-container { display: none; }'
+                    })];
             case 6:
-                body = _a.sent();
-                return [3 /*break*/, 8];
+                _a.sent();
+                return [4 /*yield*/, page.$('canvas')];
             case 7:
-                err_1 = _a.sent();
-                console.error(err_1);
-                throw new Error('Loading page timed out.');
-            case 8: return [4 /*yield*/, page.screenshot({ path: image })];
+                canvas = _a.sent();
+                console.log("Saving PNG to ".concat(screenshotPath, "..."));
+                return [4 /*yield*/, canvas.screenshot({ type: 'png', path: screenshotPath })];
+            case 8:
+                _a.sent();
+                console.log("Done!");
+                return [3 /*break*/, 12];
             case 9:
+                e_1 = _a.sent();
+                console.log(e_1);
+                return [3 /*break*/, 12];
+            case 10: return [4 /*yield*/, browser.close()];
+            case 11:
                 _a.sent();
-                return [4 /*yield*/, browser.close()];
-            case 10:
-                _a.sent();
-                return [2 /*return*/];
+                return [7 /*endfinally*/];
+            case 12: return [2 /*return*/];
         }
     });
 }); })();
 // Resources:
-// - https://code.visualstudio.com/docs/typescript/typescript-tutorial
 // - https://developer.chrome.com/blog/headless-chrome/
-// - https://developer.chrome.com/docs/puppeteer/get-started/
-//
-// - https://stackoverflow.com/questions/71656606/getting-blank-screenshot-from-puppeteer
-// - https://github.com/puppeteer/puppeteer/issues/2423
-//
-// - https://stackoverflow.com/questions/52497252/puppeteer-wait-until-page-is-completely-loaded
-// - https://medium.com/@jaredpotter1/connecting-puppeteer-to-existing-chrome-window-8a10828149e0
-// - https://github.com/puppeteer/puppeteer/issues/3543
-// - https://stackoverflow.com/questions/48218584/tell-puppeteer-to-open-chrome-tab-instead-of-window
-// - https://stackoverflow.com/questions/62852714/pyppeteer-wait-until-all-elements-of-page-is-loaded
-//
-// - https://stackoverflow.com/questions/53236692/how-to-use-chrome-profile-in-puppeteer
-// - https://stackoverflow.com/questions/57623828/in-puppeteer-how-to-switch-to-chrome-window-from-default-profile-to-desired-prof/57662769#57662769
+// - https://developer.chrome.com/docs/puppeteer/
+// - https://pptr.dev/
--- scripts/screenshot.ts
+++ scripts/screenshot.ts
@@ -8,57 +8,62 @@
 
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  // const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: true, ignoreHTTPSErrors:true});
-  
-  const page = await browser.newPage();
+// TODO - add command line args:
+// - map guid 
+// - image path
 
-  const VWIDTH = 1382;
-  const VHEIGHT = 790;
-  const VIEWPORT = { width: VWIDTH, height: VHEIGHT }; 
-  await page.setViewport(VIEWPORT);
+// const [url, pathInput] = process.argv.slice(2);
+// if (url === undefined) {
+//   console.log("Usage: node mapscreenshot.js <url> [<screenshot PNG path>]");
+// }
 
-  // const url = 'https://www.nakedcapitalism.com/';
-  const url = 'https://davesredistricting.org/';
-  // const url = 'https://davesredistricting.org/maps#viewmap::bbd90d8a-b4c3-4875-8ffe-4f931e141211';
+const url = 'https://davesredistricting.org/maps#viewmap::bbd90d8a-b4c3-4875-8ffe-4f931e141211';
+// const url = 'https://www.nakedcapitalism.com/';
+// const url = 'https://davesredistricting.org/';
 
-  const image = '/Users/alecramsay/Downloads/screenshot.png';
+const screenshotPath = "/Users/alecramsay/Downloads/screenshot.png";
+// const screenshotPath = pathInput ?? "screenshot.png";
 
-  try {
-    await page.goto(url, {
-      waitUntil: 'domcontentloaded'
-      // waitUntil: 'networkidle0'
-      // waitUntil: 'networkidle2'
-      // timeout: 10000
+(async () =>
+{
+  const browser = await puppeteer.launch({headless: "new"});
+
+  try
+  {
+    const page = await browser.newPage();
+
+    // await page.setViewport({width: 2560, height: 1600});
+    // await page.setViewport({width: 1998, height: 1394});
+    await page.setViewport({width: 1280, height: 1024});
+
+    console.log("Loading the page. This can take 30 seconds or so... ");
+    await page.goto(url, {waitUntil: ['load', 'domcontentloaded', 'networkidle0'], timeout: 0});
+
+    await page.addStyleTag({
+      content: '.MuiModal-backdrop, .MuiDialog-container, .MuiButtonBase-root, .MuiTypography-root, .mapboxgl-control-container { display: none; }'
     });
 
-    const body = await page.$("body");
-    // await page.waitForSelector('#root', {timeout: 10000});
+    const canvas = await page.$('canvas');
 
-  } catch (err) {
-    console.error(err);
-    throw new Error('Loading page timed out.');
+    console.log(`Saving PNG to ${screenshotPath}...`);
+    await canvas.screenshot({type: 'png', path: screenshotPath});
+
+    console.log("Done!");
+
+  } catch (e)
+  {
+    console.log(e)
+  } finally
+  {
+    await browser.close();
   }
-  
-  await page.screenshot({ path: image });
 
-  await browser.close();
+  // await page.screenshot({path: screenshotPath});
+
+  // await browser.close();
 })();
 
 // Resources:
-// - https://code.visualstudio.com/docs/typescript/typescript-tutorial
 // - https://developer.chrome.com/blog/headless-chrome/
-// - https://developer.chrome.com/docs/puppeteer/get-started/
-//
-// - https://stackoverflow.com/questions/71656606/getting-blank-screenshot-from-puppeteer
-// - https://github.com/puppeteer/puppeteer/issues/2423
-//
-// - https://stackoverflow.com/questions/52497252/puppeteer-wait-until-page-is-completely-loaded
-// - https://medium.com/@jaredpotter1/connecting-puppeteer-to-existing-chrome-window-8a10828149e0
-// - https://github.com/puppeteer/puppeteer/issues/3543
-// - https://stackoverflow.com/questions/48218584/tell-puppeteer-to-open-chrome-tab-instead-of-window
-// - https://stackoverflow.com/questions/62852714/pyppeteer-wait-until-all-elements-of-page-is-loaded
-//
-// - https://stackoverflow.com/questions/53236692/how-to-use-chrome-profile-in-puppeteer
-// - https://stackoverflow.com/questions/57623828/in-puppeteer-how-to-switch-to-chrome-window-from-default-profile-to-desired-prof/57662769#57662769
+// - https://developer.chrome.com/docs/puppeteer/
+// - https://pptr.dev/
