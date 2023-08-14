cd8e805c7b942e1c09820e70e92463781d2cd674
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-08-14 03:18:31 -0700

    Coloring

--- scripts/ONETIME/debug.py
+++ scripts/ONETIME/debug.py
@@ -8,12 +8,47 @@ DEBUGGING DRIVER
 import os
 from csv import DictReader
 
+import networkx as nx
+
 from pg import *
 
 
 xx: str = "NC"
 output_dir: str = os.path.expanduser("~/Downloads/NC/")
 assignments_csv: str = os.path.join(output_dir, "NC_2022_Congress_Official.csv")
+
+###
+
+
+def pick_color(id: int) -> str:
+    """Translate a 1-N color index to a color hex code.
+
+    The DRA color grid is 13 x 12 (or 12 x 12, if you skip the partial first row).
+    These are 6 colors in the Plasma colormap, repeated as necessary.
+
+    1 - #f0f823 -  1 x  1 :  0 x  1
+    2 - #0f0787 - 13 x 12 : 12 x 12
+    3 - #fca735 -  3 x 12 :  2 x 12
+    4 - #c5407e -  8 x  1 :  7 x  1
+    5 - #f48948 -  5 x  1 :  4 x  1
+    6 - #8b09a5 - 10 x  6 :  9 x  6
+
+    """
+
+    colors: list[str] = [
+        "#f0f823",
+        "#0f0787",
+        "#fca735",
+        "#c5407e",
+        "#f48948",
+        "#8b09a5",
+    ]
+    n_colors: int = len(colors)
+
+    color: str = colors[(id - 1) % n_colors]
+
+    return color
+
 
 ### ASSIGN DISTRICT COLORS, BASED ON ADJACENCY ###
 
@@ -100,5 +135,18 @@ for district in precincts_by_district:
                     and neighbor_district not in district_graph[district]
                 ):
                     district_graph[district].add(neighbor_district)
+
+## Convert the district graph to a networkx graph & assign colors to the districts
+
+G: nx.Graph = nx.Graph()
+elist: list[tuple[int, int]] = [
+    (x, y) for x in district_graph for y in district_graph[x]
+]
+G.add_edges_from(elist)
+
+d: dict[int, int] = nx.coloring.greedy_color(G)
+# d = nx.coloring.greedy_color(G, strategy="largest_first")
+
+district_colors: dict[int, str] = {k: pick_color(v) for k, v in d.items()}
 
 pass
